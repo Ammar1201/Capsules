98653ca43eea6b21dba007b767a93cad180579f3
Ammar1201 <aamour1201@gmail.com>
2022-10-28 15:08:19 +0300

    added data table and basic styling

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 
 //* https://capsules7.herokuapp.com/api/group/:number
 
+const table = document.querySelector('#container');
+
+let lightORdark = true; //* true - light class, false - dark class
+
 const fetchData = async (url) => {
   try {
     const res = await fetch(url);
@@ -29,3 +33,56 @@ const getData = async () => {
   }
   return await Promise.all(students);
 }
+
+const createRow = () => {
+  const row = document.createElement('div');
+
+  if(lightORdark) {
+    row.classList.add('light-row');
+    lightORdark = !lightORdark;
+  }
+  else {
+    row.classList.add('dark-row');
+    lightORdark = !lightORdark;
+  }
+
+  table.appendChild(row);
+  
+  const IDlabel = document.createElement('label');
+  IDlabel.classList.add('font');
+  IDlabel.textContent = 'test'
+  row.appendChild(IDlabel);
+
+  for(let i = 0; i < 7; i++) {
+    const span = document.createElement('span');
+    span.classList.add('font');
+    span.textContent = 'test';
+    row.appendChild(span);
+  }
+
+  const div = document.createElement('div');
+  div.classList.add('btns');
+
+  const editBtn = document.createElement('button');
+  editBtn.textContent = 'Edit';
+  editBtn.classList.add('light-button');
+  div.appendChild(editBtn);
+  
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.classList.add('dark-button');
+  div.appendChild(deleteBtn);
+
+  row.appendChild(div);
+}
+
+const displayData = async () => {
+  // const data = await getData();
+
+  const row = createRow();
+
+  // appendData(data, row);
+  appendData(row);
+}
+
+displayData();
